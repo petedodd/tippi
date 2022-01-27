b49648c0d274e985fe1dc150dab57ff3550fabae
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2022-01-27 14:46:34 +0000

    tidying results uploader

--- resultsupload.R
+++ resultsupload.R
@@ -6,38 +6,24 @@ library(googlesheets4)
 
 ## NOTE new sheet
 ## create an ID to access the googlesheets results sheet
-"https://docs.google.com/spreadsheets/d/1yRUIErcA9H_1rbgG46WoKARt9-8t94tnJRS3X91gzFo/edit#gid=0" %>%
-  as_sheets_id() %>%
-  as.character() -> shid
+yourl <- "https://docs.google.com/spreadsheets/d/1yRUIErcA9H_1rbgG46WoKARt9-8t94tnJRS3X91gzFo/edit#gid=0"
+shid <- as.character(as_sheets_id(yourl))
 
 
-## utility functions
-see <- function(x,ns=3)formatC(signif(x,ns),big.mark = ",",format='fg') #for reading big numbers
-r1 <- function(x) sprintf("%.1f", round(x,1) )
+## utility function
+upload.to.sheets <- function(basename,filename,sheetid,
+                             sap #pulmonary sa?
+                             ){
+  fn <- glue(basename) + filename + ".csv"
+  tmp <- fread(file=fn)
+  write_sheet(tmp,sheetid,sheet=filename)
+}
 
-
-## reformatting etc needed
-cascadetab <- fread(file=here('model/outdata/cascadetab.csv'))
-write_sheet(cascadetab,shid,sheet='test')
-
-
-## ATT results
-fn1 <- glue(here('model/outdata/ICERatt')) + SAT + '.csv'
-icer <- fread(fn1)
-
-sn <- glue('ICERatt{SAT}.csv')
-write_sheet(icer,shid,sheet=sn)
-
-
-fn1 <- glue(here('outdata/ICERSatt')) + SAT + '.csv'
-icers <- fread(file=fn1)
-
-sn <- glue('ICERSatt{SAT}.csv')
-write_sheet(icers,shid,sheet=sn)
-
-
-## TODO
-fwrite(hago,file=here('outdata/PTC.csv')) #save as output too
+## upload relevant table data
+upload.to.sheets(here('model/outdata/'),'cascadetab',shid)
+upload.to.sheets(here('model/outdata/'),'ICERatt',shid)
+upload.to.sheets(here('model/outdata/'),'ICERSatt',shid)
+upload.to.sheets(here('model/outdata/'),'PTC',shid)
 
 
 

00130810d2012cfe820fb5f1996818cc68de351f
petedodd <p.j.dodd@sheffield.ac.uk>
2022-02-09 08:40:58 +0000

    updating graphing

--- inference/tippigather.R
+++ inference/tippigather.R
@@ -2,37 +2,82 @@
 library(here)
 library(data.table)
 library(ggplot2)
+library(scales)
 library(ggpubr)
+library(glue)
+gh <- function(x) glue(here(x))
+dg <- 1
+ft <- function(x)format(x,digits=dg,nsmall=dg)
 
-## load graph data:
+## make single graphs
 grphs <- list()
-load(here('data/txMA304.Rdata'))
-grphs[['tx04']] <- MAP
-load(here('data/pxMA304.Rdata'))
-grphs[['px04']] <- MAP
-load(here('data/txMA3514.Rdata'))
-grphs[['tx514']] <- MAP
-load(here('data/pxMA3514.Rdata'))
-grphs[['px514']] <- MAP
-load(here('data/txMA3014.Rdata'))
-grphs[['tx014']] <- MAP
-load(here('data/pxMA3014.Rdata'))
-grphs[['px014']] <- MAP
+for(page in 1:3){
+  for(qty in c("tx","px")){
+    cat(qty,"...\n")
+    ## --- read data
+    ## empirical data
+    ## sites
+    siteeffects <- D[,.(site.effect = (Intervention.Num/
+                                       Intervention.FT)/
+                          (Baseline.Num/Baseline.FT),
+                        int.number = Intervention.Num,
+                        country=Country,
+                        index)]
+    ## country
+    countryeffects <- D[,.(country.effect =
+                             (sum(Intervention.Num)/
+                              sum(Intervention.FT))/
+                             (sum(Baseline.Num)/sum(Baseline.FT)),
+                           int.number=sum(Intervention.Num) ),
+                        by=.(country=Country)]
+    ## MA results
+    MC <- fread(gh('outdata/{qty}MC{shhs[page,age]}.csv'))
+    MC$country <- factor(MC$country,
+                         levels=rev(MC$country),ordered = TRUE)
+    MC[,txt:=paste0(ft(`50%`)," (",ft(`2.5%`)," - ",ft(`97.5%`),")")]
 
+    ## --- make graph
+    ## title
+    ttl <- glue(ifelse(qty=='tx',
+                       'anti-tuberculosis treatment initiation',
+                       'tuberculosis preventive therapy initiation')) +
+      ", " + shhs[page,aged] + ' years'
+    ## graph
+    MAP <-
+      ggplot(MC,aes(country,`50%`)) +
+      geom_point(size=2) +
+      geom_point(data=siteeffects[is.finite(site.effect)],
+                 aes(country,site.effect,
+                     size=int.number,col=country),
+                 shape=1) +
+      geom_point(data=countryeffects,aes(country,country.effect,
+                                         size=int.number,col=country),
+                 shape=4) +
+      geom_hline(yintercept = 1,lty=2,col='darkgrey')+
+      geom_point(size=2) +
+      geom_errorbar(aes(ymin=`2.5%`,ymax=`97.5%`),width=0) +
+      geom_text(aes(x=country,y=90,label=txt),size=3)+
+      scale_y_log10(limits=c(5e-2,1.4e2),
+                    label=comma) + #NOTE one DRC site omitted
+      ylab('Rate ratio (log scale)')+
+      xlab('Country')+
+      coord_flip() +
+      theme_classic() + ggpubr::grids()+ ggtitle(ttl)+
+      labs(size='Number (intervention)')+
+      guides(colour="none") + 
+      theme(legend.position = 'bottom')
 
+    ## save
+    grphs[[glue("{qty}{shhs[page,age]}")]] <- MAP
+  }
+}
 ## make joined graph
-ggarrange(plotlist = grphs,
-          ncol=2,nrow=3,
-          labels = paste0(letters[1:6],")"),
-          common.legend = TRUE,legend='top')
-
-ggsave(filename=here("graphs/MAll.eps"),w=10,h=12)
-ggsave(filename=here("graphs/MAll.png"),w=10,h=12)
-
-## TODO
-## additional titles on graphs
-## log scale
-## text on graphs
+GA <- ggarrange(plotlist = grphs,
+                ncol=2,nrow=3,
+                labels = paste0(letters[1:6],")"),
+                common.legend = TRUE,legend='top')
+ggsave(filename=here("graphs/MAll.eps"),w=13,h=12)
+ggsave(GA,filename=here("graphs/MAll.png"),w=13,h=12)
 
 
 ## gather summaries for reporting

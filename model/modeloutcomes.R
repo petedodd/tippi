94f6ae237e3e8bda5a5fcb75e2e4fa5397f52740
Pete Dodd <petedodd@Petes-Mac-Pro.home>
2022-06-10 09:05:00 +0100

    figure 3 draft

--- model/modeloutcomes.R
+++ model/modeloutcomes.R
@@ -29,6 +29,7 @@ library(data.table)
 library(HEdtree)
 library(ggplot2)
 library(ggthemes)
+library(ggpubr)
 library(scales)
 library(glue)
 
@@ -1233,8 +1234,6 @@ fwrite(picers,file=fn)
 
 ## TODO cross check cost SOC PT - seems different
 
-PTT[,unique(iso3)]
-
 ## ================= BOTH components ============================
 ## want weighting during baseline
 PTT <- merge(T1[,.(country,iso3,id,Dcost.att=Dcost,dDALY.att=dDALY)],
@@ -1266,23 +1265,23 @@ ggsave(GP,file=fn1,w=10,h=10); ## ggsave(GP,file=fn2,w=10,h=10)
 
 ## make CEAC data
 lz <- seq(from = 0,to=ceactop,length.out = 1000)
-pceacd <- list()
+bceacd <- list()
 for(iso in unique(PT1$iso3)){
     tmp <- PTT[iso3==iso,.(Q=dDALY,P=Dcost)]
-    pceacd[[iso]] <- data.table(
+    bceacd[[iso]] <- data.table(
         iso3=iso,x=lz,
         y=make.ceac(tmp,lz))
 }
-pceacd <- rbindlist(pceacd)
+bceacd <- rbindlist(bceacd)
 
 ## make CEAC plot
-PCEAC <- make.ceac.plot(pceacd,xpad=50)
-if(!shell) PCEAC
+BCEAC <- make.ceac.plot(bceacd,xpad=50)
+if(!shell) BCEAC
 
 ## save out
 fn1 <- glue(here('graphs/CEACall')) + SA + '.' + ACF + '.png'
 ## fn2 <- glue(here('graphs/CEACall')) + SA + '.' + ACF + '.pdf'
-ggsave(PCEAC,file=fn1,w=10,h=10); ## ggsave(PCEAC,file=fn2,w=10,h=10)
+ggsave(BCEAC,file=fn1,w=10,h=10); ## ggsave(PCEAC,file=fn2,w=10,h=10)
 
 
 ##  combined ICERS
@@ -1390,10 +1389,28 @@ Table2both <- bicer
 fn <- glue(here('outdata/Table2both')) + SA +'.' + ACF + '.Rdata'
 save(Table2both,file=fn)
 
+## ---- combined CEAC plot ---
+## CEAC #from ceacd
+## PCEAC #from pceacd
+## BCEAC #from bceacd
+## TODO country names not iso3
+
+ttls <- c('Intensified case-finding intervention',
+          'Household contact management intervention',
+          'Combined CaP TB intervention package')
+CAP <- list(CEAC,PCEAC,BCEAC)
+for(i in 1:3) CAP[[i]] <- CAP[[i]] + ggtitle(ttls[i])
+
+CAPP <- ggarrange(plotlist = CAP,
+                  labels="AUTO",
+          common.legend = TRUE,legend="bottom",ncol=1)
+
+fn1 <- glue(here('graphs/allCEACs')) + SA + '.' + ACF + '.png'
+## fn2 <- glue(here('graphs/allCEACs')) + SA + '.' + ACF + '.pdf'
+ggsave(CAPP,file=fn1,w=8,h=15); #ggsave(CAPP,file=fn2,w=8,h=15);
 
 
 ## ================= TODO list ============================
-## check X-ray costing
 
 ## NOTE
 ## SHARED tippi folder:
